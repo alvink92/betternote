ff5e32ed4a97e3adc19c5ef6e22198d642ea6944
Alvin Kong <alvink92@gmail.com>
2017-09-19 11:34:01 -0700

    Added Procfile

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-const App = () => <div>App</div>;
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
