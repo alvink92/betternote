55ecdb9239d3fd24b6dec4d8eb1f337f74450803
Alvin Kong <alvink92@gmail.com>
2017-09-20 14:39:35 -0700

    Half way complete with splash page content

--- frontend/components/homepage/content.jsx
+++ frontend/components/homepage/content.jsx
@@ -8,7 +8,21 @@ class Content extends React.Component {
   render() {
     return (
       <div className="homepage-content">
-        <div>content</div>
+        <div className="desc-section">
+          <section>
+            <h1>Meet Betternote, your second brain.</h1>
+            <p>
+              Capture, organize, and share notes from anywhere. Your best ideas
+              are always with you and always in sync.
+            </p>
+          </section>
+        </div>
+        <section className="content-divider">
+          <div className="v-line"> </div>
+        </section>
+        <div className="registration-form-container">
+          <h1>Sign Up for Free</h1>
+        </div>
       </div>
     );
   }
--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -7,11 +7,22 @@ class Homepage extends React.Component {
     super(props);
   }
 
+  vGap(height) {
+    const divStyle = {
+      display: "block",
+      width: "100%",
+      clear: "both",
+      height: height
+    };
+    return <div style={divStyle} />;
+  }
+
   render() {
     return (
       <div className="homepage-container">
         <Navbar />
         <div className="content-container inner-margin">
+          {this.vGap("85px")}
           <Content />
         </div>
       </div>
