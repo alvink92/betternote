962aa95b34fa3467c71eef7685248619818cc362
Alvin Kong <alvink92@gmail.com>
2017-09-23 21:18:36 -0700

    Finished UI for tags sliding panel and delete handlers. now to do update handler

--- frontend/components/tags/tag_index_item.jsx
+++ frontend/components/tags/tag_index_item.jsx
@@ -5,17 +5,69 @@ class TagIndexItem extends React.Component {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleUpdateClick = this.handleUpdateClick.bind(this);
   }
+
+  componentWillMount() {
+    this.setState({ editable: false, tagName: this.props.tag.name });
+  }
+
   handleClick(e) {
-    if (e.target.className !== "") {
+    if (
+      e.target.className === "tag-item" ||
+      e.target.className === "tag-name" ||
+      e.target.className === "tag-count"
+    ) {
       this.props.history.push(`/tags/${this.props.tag.id}`);
+    }
+  }
+
+  handleUpdateClick(e) {
+    this.setState({ editable: true });
+  }
+
+  handleDeleteClick(e) {
+    e.preventDefault();
+    this.props.deleteTag(this.props.tag.id);
+  }
+
+  hiddenModButtons() {
+    return (
+      <div className="mod-btn-container hidden">
+        <div className="tag-edit" onClick={this.handleUpdateClick}>
+          <i className="fa fa-pencil" aria-hidden="true" />
+        </div>
+        <div className="tag-delete" onClick={this.handleDeleteClick}>
+          <i className="fa fa-trash-o" aria-hidden="true" />
+        </div>
+      </div>
+    );
+  }
+
+  tagSwitch() {
+    if (this.state.editable) {
+      return (
+        <div className="editable">
+          <input />
+        </div>
+      );
+    } else {
+      return (
+        <span className="tag-item">
+          <div className="tag-name">{this.state.tagName}</div>
+          <div className="tag-count">{this.props.tag.noteIds.length}</div>
+        </span>
+      );
     }
   }
 
   render() {
     return (
-      <div className="tag-name-container" onClick={this.handleClick}>
-        {this.props.tag.name}
+      <div className="tag-item-container" onClick={this.handleClick}>
+        {this.tagSwitch()}
+        <div className="hack hidden" />
+        {this.hiddenModButtons()}
       </div>
     );
   }
