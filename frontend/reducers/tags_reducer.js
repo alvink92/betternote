f6f5f227286586566f3da6482b95e997503b4c67
Alvin Kong <alvink92@gmail.com>
2017-09-30 09:57:33 -0700

    minor fixes

--- frontend/reducers/tags_reducer.js
+++ frontend/reducers/tags_reducer.js
@@ -5,6 +5,7 @@ import {
   RECEIVE_TAGGING,
   REMOVE_TAGGING
 } from "../actions/tag_actions";
+// import { RECEIVE_NOTE } from "../actions/note_actions";
 import merge from "lodash/merge";
 
 const tagsReducer = (state = {}, action) => {
@@ -24,11 +25,21 @@ const tagsReducer = (state = {}, action) => {
     case RECEIVE_TAGGING:
       newState = merge({}, state);
       const tag = action.tagging.tag;
-      // if break maybe here
       if (!newState[tag.id]) {
         newState[tag.id] = tag;
       }
       return newState;
+    // case RECEIVE_NOTE:
+    //   newState = merge({}, state);
+    //   const note = action.note;
+    //   const tags = note.tags;
+    //
+    //   tags.map(currTag => currTag.id).forEach(tagId => {
+    //     if (!newState[tagId].noteIds.includes(note.id)) {
+    //       newState[tagId].noteIds = newState[tagId].noteIds.push(note.id);
+    //     }
+    //   });
+    //   return newState;
     default:
       return state;
   }
