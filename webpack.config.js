9850253dbc11e8b530ca4d6a03954c62e80560e8
Alvin Kong <alvink92@gmail.com>
2017-09-30 10:03:00 -0700

    added plugins to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
@@ -22,5 +23,18 @@ module.exports = {
   devtool: "source-maps",
   resolve: {
     extensions: [".js", ".jsx", "*"]
-  }
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
+    })
+  ]
 };
