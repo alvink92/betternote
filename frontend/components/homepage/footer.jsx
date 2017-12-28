a362073704ba653693b91d3fcef1ad4a967c78af
Alvin Kong <alvink92@gmail.com>
2017-12-27 21:13:58 -0800

    added nav section to footer

--- frontend/components/homepage/footer.jsx
+++ frontend/components/homepage/footer.jsx
@@ -6,14 +6,100 @@ class Footer extends React.Component {
   }
 
   render() {
-    let footerNav = (
-      <div className="footer-wrap nav">
-        <div className="home-inner-width" />
-      </div>
-    );
 
-    return (
-      <div className="footer-container">
+    return <div className="footer-container">
+        <div className="footer-wrap nav">
+          <div className="home-inner-width link-section-wrap">
+            <div className="link-section">
+              <p className="link-section-header">Plans</p>
+              <ul>
+                <li>
+                  <a href="/">Getting started</a>
+                </li>
+                <li>
+                  <a href="/">Basic</a>
+                </li>
+                <li>
+                  <a href="/">Plus</a>
+                </li>
+                <li>
+                  <a href="/">Premium</a>
+                </li>
+              </ul>
+            </div>
+            <div className="link-section">
+              <p className="link-section-header">Business</p>
+              <ul>
+                <li>
+                  <a href="/">Features</a>
+                </li>
+                <li>
+                  <a href="/">Use cases</a>
+                </li>
+                <li>
+                  <a href="/">Customer stories</a>
+                </li>
+                <li>
+                  <a href="/">Contact sales</a>
+                </li>
+              </ul>
+            </div>
+            <div className="link-section">
+              <p className="link-section-header">Discover</p>
+              <ul>
+                <li>
+                  <a href="/">Blog</a>
+                </li>
+                <li>
+                  <a href="/">Community</a>
+                </li>
+                <li>
+                  <a href="/">Certified consultants</a>
+                </li>
+                <li>
+                  <a href="/">Events</a>
+                </li>
+                <li>
+                  <a href="/">App center</a>
+                </li>
+                <li>
+                  <a href="/">Web clipper</a>
+                </li>
+              </ul>
+            </div>
+            <div className="link-section">
+              <p className="link-section-header">Support</p>
+              <ul>
+                <li>
+                  <a href="/">Help & Learning</a>
+                </li>
+                <li>
+                  <a href="/">Support</a>
+                </li>
+                <li>
+                  <a href="/">Forum</a>
+                </li>
+                <li>
+                  <a href="/">Developers</a>
+                </li>
+              </ul>
+            </div>
+            <div className="link-section">
+              <p className="link-section-header">Company</p>
+              <ul>
+                <li>
+                  <a href="/">Careers</a>
+                </li>
+                <li>
+                  <a href="/">Press</a>
+                </li>
+                <li>
+                  <a href="/">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
         <div className="footer-wrap social">
           <div className="home-inner-width">
             <ul className="social-links">
@@ -47,8 +133,7 @@ class Footer extends React.Component {
             </p>
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
