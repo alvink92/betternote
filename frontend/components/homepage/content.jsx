379e17a5dd8fde0d9dcd85d6d729b7c32d4204fd
Alvin Kong <alvink92@gmail.com>
2017-12-29 13:35:07 -0800

    responsive content setion

--- frontend/components/homepage/content.jsx
+++ frontend/components/homepage/content.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SessionForm from "../session/session_form";
+import { Link } from "react-router-dom";
 
 class Content extends React.Component {
   constructor(props) {
@@ -7,14 +8,13 @@ class Content extends React.Component {
   }
 
   render() {
-    return (
-      <div className="homepage-content home-inner-width">
+    return <div className="homepage-content home-inner-width">
         <div className="desc-section">
           <section>
             <h1>Meet Betternote, your second brain.</h1>
             <p>
-              Capture, organize, and share notes from anywhere. Your best ideas
-              are always with you and always in sync.
+              Capture, organize, and share notes from anywhere. Your best
+              ideas are always with you and always in sync.
             </p>
           </section>
         </div>
@@ -24,16 +24,12 @@ class Content extends React.Component {
         <div className="registration-form-container">
           <h1>Sign Up for Free</h1>
           <br />
-          <SessionForm
-            errors={this.props.errors}
-            formTypePath="/signup"
-            processForm={this.props.processForm}
-            login={this.props.login}
-            history={this.props.history}
-          />
+          <SessionForm errors={this.props.errors} formTypePath="/signup" processForm={this.props.processForm} login={this.props.login} history={this.props.history} />
         </div>
-      </div>
-    );
+        <Link to="/signup" className="betternote-btn disp-block-resp-max-960 hidden" style={{margin: "0", width: "225px"}}>
+          Create your free account
+        </Link>
+      </div>;
   }
 }
 
